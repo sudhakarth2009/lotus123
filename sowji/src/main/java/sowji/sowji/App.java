9e1e35f61ec02e389f0f1d0b610cc2f0bcee4724
LENOVO <LENOVO@DESKTOP-07LUTLC>
2020-03-12 10:47:21 +0530

    hi sowji

---
+++ sowji/src/main/java/sowji/sowji/App.java
@@ -0,0 +1,13 @@
+package sowji.sowji;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
